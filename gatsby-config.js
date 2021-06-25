e7365a3feed6b454c82cd94f88e578e6249970c3
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-06-25 12:08:17 +0530

    old files checking

---
+++ gatsby-config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  siteMetadata: {
+    title: `Backtoroad Autoparts`,
+    description: `BackToRoad Auto Parts is your one-stop shop for tested high quality used OEM parts to customers in all 50 states and worldwide`,
+    author: `@backtoroad`,
+    noindex: `noindex`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+    // {
+    //   resolve: 'gatsby-plugin-react-svg',
+    //   options: {
+    //     rule: {
+    //       include: `${__dirname}/src/assets/images`,
+    //     }
+    //   }
+    // }
+    // // Simple config, passing URL
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     // Arbitrary name for the remote schema Query type
+    //     typeName: "WORDPRESS",
+    //     // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+    //     fieldName: "wordpress",
+    //     // Url to query from
+    //     url: "http://stockeye.in/graphql",
+    //   },
+    // },
+  ],
+}
