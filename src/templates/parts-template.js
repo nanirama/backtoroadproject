ab5ad73432e913f24bfeb194833b7ed28b926541
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-06-26 07:08:31 +0530

    changes 26-06 4

--- src/templates/parts-template.js
+++ src/templates/parts-template.js
@@ -1,11 +1,25 @@
-import React from "react"
-import { graphql} from "gatsby"
+import React, { Component } from 'react';
+import Carousel from 'react-elastic-carousel';
 
-const PartsTemplate = (props) => {
-	return(
-        <h2>Welcome</h2>
+class PartsTemplate extends Component {
+  state = {
+    items: [
+      {id: 1, title: 'item #1'},
+      {id: 2, title: 'item #2'},
+      {id: 3, title: 'item #3'},
+      {id: 4, title: 'item #4'},
+      {id: 5, title: 'item #5'}
+    ]
+  }
+
+  render () {
+    const { items } = this.state;
+    return (
+      <Carousel>
+        {items.map(item => <div key={item.id}>{item.title}</div>)}
+      </Carousel>
     )
+  }
 }
-
 export default PartsTemplate
   
