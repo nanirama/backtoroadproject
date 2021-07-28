5004fcbb51326d3a3deb75da26ea41c539a632e0
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-07-28 12:23:49 +0530

    changes 4

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,3 +4,11 @@ const Layout = require("./src/layout/layout").default
 exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  replaceHeadComponents(removePreload(getHeadComponents()))
+}
+
+function removePreload (components) {
+  return components.filter((component) => component.type !== 'link' && component.props && component.props.rel !== 'preload')
+}
