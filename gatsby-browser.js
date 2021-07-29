40ce131d75947cc4e2990e6724f2e3265a7f94b7
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-07-29 19:54:17 +0530

    add seo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './src/layout/layout.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "babel-polyfill";
 
-// export const onClientEntry = async () => {
-//     if (typeof IntersectionObserver === "undefined") {
-//       await import("intersection-observer")
-//     }
-//   }
+export const onClientEntry = async () => {
+    if (typeof IntersectionObserver === "undefined") {
+      await import("intersection-observer")
+    }
+  }
