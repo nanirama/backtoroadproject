4c6e826f131e3cec7d55a603b5413a513c782231
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-07-31 18:43:39 +0530

    changes 13

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -2,9 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 const Layout = ({ children }) => {
   return (
-    <div className="border2">
       {children}
-      </div>
   )
 }
 Layout.propTypes = {
