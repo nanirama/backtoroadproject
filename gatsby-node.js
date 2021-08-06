d8499b24cb7e3fc7ae927f336f6b6488148066ec
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-08-06 20:08:58 +0530

    all pages changes 11

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,6 @@ exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;
 
     return new Promise((resolve, reject) => {
-
      graphql(`
         query{
             allPosts : allWpPost {
@@ -127,6 +126,7 @@ exports.createPages = async ({ graphql, actions }) => {
       const posts = _l.cloneDeep(result.data.allPosts.edges);
       const numPages = Math.ceil(posts.length / postsPerPage)
       Array.from({ length: numPages }).forEach((_, i) => {
+        console.log('Page Number is ', i);
       createPage({
         path: i === 0 ? `/blog/` : `/blog/${i + 1}`,
         component: path.resolve("./src/templates/blog-list-template.js"),
