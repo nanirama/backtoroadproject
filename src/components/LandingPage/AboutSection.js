b2ce0a756b1645da3dcac41b1eb0f0fb8ad96c64
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-08-09 10:15:21 +0530

    changes 09-08 3

--- src/components/LandingPage/AboutSection.js
+++ src/components/LandingPage/AboutSection.js
@@ -24,7 +24,11 @@ const AboutSection = ({ image }) => {
                 <h2 className="tlt mb-2 pb-2 mt-3 text-uppercase">About BackToRoad Auto Parts</h2>
                 <p>BackToRoad Auto Parts is your go-to place for high-quality used OEM auto parts. We have an unrivaled selection of used auto parts for sale from a variety of cars, including even the latest models. We make sure to sell only high-quality and genuine OEM parts that are inspected by our Auto Parts team and verified to be in good working condition. </p>
                 <p>All of our products come with a 30-day warranty so our customers are guaranteed a pleasant experience each time they shop with us. We also offer free and quick delivery of our products to our customers across the USA and Canada. We ship either to your doorstep or directly to your mechanic as per your convenience, allowing you to get your car back on the road as soon as possible. </p>
-                <Link to="/about" className="btn2">Check Out <BsArrowRight style={iconStyles}/></Link> </div>
+                <Link to="/about" className="btn2">Check Out 
+     <svg viewBox="0 0 500 500" >
+       <path d="M357,214v-64a9,9 0 01 15-5l106,96.5a9.5,9.5 0 01 0,15.5l-106,96.5a9,9 0 01-15-5v-64h-348a9,9 0 01 -9-9v-52.5a9,9 0 01 9-9z" />
+    </svg>
+  </Link> </div>
             </div>
             </div>
         </div>
