0fffab433dee36b05f6dfe0afc8c45de12a4ce15
Thammaji Rao Nalla <sellectricdeveloper@gmail.com>
2021-08-16 11:06:42 +0530

    doc 5 changes

--- src/components/LandingPage/Breadcrumbs.js
+++ src/components/LandingPage/Breadcrumbs.js
@@ -8,48 +8,27 @@ const LandingBreadcrumbs = (props) => {
             <ol vocab="https://schema.org/" typeof="BreadcrumbList" itemscope itemScope itemtype="https://schema.org/BreadcrumbList">
             { bcrumb.map((item, index)=>{
                 return(
-                    <>
-                
+                <li property="itemListElement" typeof="ListItem">
                 { item.link != null &&  (
-                <li
-                    key={index}
-                    itemprop="itemListElement"                
-                    itemScope
-                    itemtype="https://schema.org/ListItem"
-                    property="itemListElement"
-                    typeof="ListItem"
-                >     
+                   <>
                     {item.itemtype && (<Link
-                        itemprop="item" 
-                        property="item"
-                        typeof="WebPage"
+                        property="item" typeof="WebPage"
                         to={item.link}                                               
                     ><span itemprop="name" property="name">{item.name}</span></Link>)}     
                     {!item.itemtype && (<Link
-                        itemprop="item"
-                        property="item"
-                        typeof="WebPage"
+                        property="item" typeof="WebPage"
                         to={item.link}                        
-                    ><span itemprop="name" property="name">{item.name}</span></Link>)}     
-                    <meta property="position" itemprop="position" content={index+1} />
-                    </li> 
+                    ><span property="name">{item.name}</span></Link>)}     
+                    <meta property="position" content={index+1} />
+                   </>
                     ) } 
                     { item.link === null &&  (
-                        <li
-                            key={index}
-                            itemprop="itemListElement"
-                            itemscope
-                            itemScope
-                            itemtype="https://schema.org/ListItem"
-                            className="active"
-                            property="itemListElement"
-                            typeof="ListItem"
-                        >
-                        <span itemprop="name" property="name">{item.name}</span>
-                        <meta property="position" itemprop="position" content={index+1} />
-                    </li>
+                       <>
+                        <span property="name">{item.name}</span>
+                        <meta property="position" content={index+1} />
+                        </>
                     )}
-                    </>
+                    </li>
                 )
             })}
             </ol>
